a1e49a2f8972c6aec22c49c200b307e402d1a3e9
George Kokkin <kokkin.george@gmail.com>
2023-03-11 10:49:04 +0000

    HW 7. React routing - Paginate SW app with routes - Add a Home page with a general description of the project - Add a Login page with the simplest login (for example, a checkbox) - Add error page - Only logged in user can watch our application - All except the Home page should have a button to return to the previous page
    https://lms.ithillel.ua/groups/63871a3b62c08578b6a65ee3/homeworks/64036b8bbeefc424b48a63d8

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/sw-app">
     {/*<React.StrictMode>*/}
     <App />
     {/*</React.StrictMode>*/}
